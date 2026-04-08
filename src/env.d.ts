aeeeaa890534ea0aa0dbb3c1b2bb52fd0dd40203
Daniel Duarte <danielduartemora27@gmail.com>
2026-04-08 18:31:24 +0200

    style: format HeadingText component and fix newline in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,2 @@
-/// <reference types="astro/client" /> 
-// esto lo agrego mi agente por un problema que me daba con el tag nav muy raro eso, 
+/// <reference types="astro/client" />
+// esto lo agrego mi agente por un problema que me daba con el tag nav muy raro eso,
