29d16db4c8d1cf153231980185df61dcbea5fe26
Santi-Burgos <santiburgos38@gmail.com>
2026-04-12 12:05:08 -0300

    fix: ceo test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://landing-page-02.example.com",
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
